7a3f2dede29bab347b948ce51f878e9e9ec16780
cryptobank04 <cryptobank04@gmail.com>
2023-08-15 16:39:10 -0400

    changed Component Name for SiroButton

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,7 @@
 import { useEffect } from 'react';
-import { Button, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import * as SiroReactNative from 'siro-react-native';
-
-console.log(SiroReactNative)
-
-
 
 export default function App() {
   useEffect(() => {
@@ -13,12 +9,7 @@ export default function App() {
   }, [])
 
   const setupSiro = () => {
-    console.log('Setting up Siro')
     SiroReactNative.setup('staging');
-  }
-
-  const showSiro = () => {
-    SiroReactNative.show();
   }
 
   const startRecording = () => {
@@ -35,20 +26,19 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>{SiroReactNative.hello()}</Text>
-      <Button title="Step 1 - Set up Siro!" onPress={setupSiro} />
-      <Button title='Step 2 - Login to Siro' onPress={showModal} />
-      <Button title="Step 3 - Start Recording!" onPress={startRecording} />
+      <Text style={{ marginBottom: 20 }}>SiroSDK Example App</Text>
+      <Button title='Step 1 - Login to Siro' onPress={showModal} />
+      <Button title="Step 2 - Start Recording!" onPress={startRecording} />
 
-      <Text>Test Events</Text>
-      <Button title="Step 4 - Send event!" onPress={() => sendEvent('Random Event', { random: 'data', someNumber: 5, somebool: true, someArray: ['rrr', 2, true] })} />
-      <Button title="Step 5 - Send dropPin event!" onPress={() => sendEvent('dropPin', { random: 'data', someNumber: 5, somebool: true, someArray: ['rrr', 2, true] })} />
-      <Button title="Step 6 - Send createLead event!" onPress={() => sendEvent('createLead', { random: 'data', someNumber: 5, somebool: true, someArray: ['rrr', 2, true] })} />
-      <Button title="Step 7 - Send leadClosed event!" onPress={() => sendEvent('leadClosed', { random: 'data', someNumber: 5, somebool: true, someArray: ['rrr', 2, true] })} />
+      <Text style={{ marginBottom: 20 }}>Test Events</Text>
+      <Button title="Step 3 - Send event!" onPress={() => sendEvent('Random Event', { random: 'data', someNumber: 5, somebool: true, someArray: ['rrr', 2, true] })} />
+      <Button title="Step 4 - Send dropPin event!" onPress={() => sendEvent('dropPin', { random: 'data', someNumber: 5, somebool: true, someArray: ['rrr', 2, true] })} />
+      <Button title="Step 5 - Send createLead event!" onPress={() => sendEvent('createLead', { random: 'data', someNumber: 5, somebool: true, someArray: ['rrr', 2, true] })} />
+      <Button title="Step 6 - Send leadClosed event!" onPress={() => sendEvent('leadClosed', { random: 'data', someNumber: 5, somebool: true, someArray: ['rrr', 2, true] })} />
 
 
       <TouchableOpacity style={{ padding: 35 }} onPress={showModal}>
-        <SiroReactNative.SiroReactNativeView style={{ backgroundColor: 'green' }} />
+        <SiroReactNative.SiroButton />
       </TouchableOpacity>
     </View>
   );
--- src/SiroReactNativeView.tsx
+++ src/SiroReactNativeView.tsx
@@ -1,11 +1,9 @@
 import { requireNativeViewManager } from 'expo-modules-core';
 import * as React from 'react';
 
-import { SiroReactNativeViewProps } from './SiroReactNative.types';
-
-const NativeView: React.ComponentType<SiroReactNativeViewProps> =
+const NativeView: React.ComponentType<{}> =
   requireNativeViewManager('SiroReactNative');
 
-export default function SiroReactNativeView(props: SiroReactNativeViewProps) {
+export default function SiroButton(props: {}) {
   return <NativeView {...props} />;
 }
