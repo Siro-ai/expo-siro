60502d29668f9661389ba0605a10d121212b5496
cryptobank04 <cryptobank04@gmail.com>
2023-09-05 08:45:42 -0400

    some updates

--- example/App.tsx
+++ example/App.tsx
@@ -1,71 +1,86 @@
-import { useEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-import * as SiroReactNative from 'siro-react-native';
+import * as SiroReactNative from "siro-react-native";
 
 const interaction: SiroReactNative.Interaction = {
-  id: '123',
+  id: "123",
 
-  userId: 'externalId',
-  note: 'Got the sale!',
+  userId: "externalId",
+  note: "Got the sale!",
   leadDateCreated: new Date(),
   dateCreated: new Date(),
   contacts: [],
-  leadId: 'leadId',
+  leadId: "leadId",
   coordinates: { latitude: 0, longitude: 0 },
   stage: {
-    id: 'stageId',
-    name: 'stageName',
-    color: '#000',
-    icon: 'icon.jpg',
+    id: "stageId",
+    name: "stageName",
+    color: "#000",
+    icon: "icon.jpg",
     won: true,
     interacted: true,
   },
   metadata: {
-    some: 'data',
+    some: "data",
     someNumber: 5,
     somebool: true,
-    someArray: ['rrr', 2, true],
-  }
-}
+    someArray: ["rrr", 2, true],
+  },
+};
 
 export default function App() {
   useEffect(() => {
-    setupSiro()
-  }, [])
+    setupSiro();
+  }, []);
 
   const setupSiro = () => {
-    SiroReactNative.setup(SiroReactNative.Environment.staging);
-  }
+    SiroReactNative.setup(SiroReactNative.Environment.production);
+  };
 
   const startRecording = () => {
     SiroReactNative.startRecording();
-  }
+  };
 
   const stopRecording = () => {
     SiroReactNative.stopRecording();
-  }
+  };
 
   const sendEvent = (eventName: string, data?: SiroReactNative.Interaction) => {
     SiroReactNative.sendEvent(eventName, data);
-  }
+  };
 
   const showModal = () => {
     SiroReactNative.showModal();
-  }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={{ marginBottom: 20 }}>SiroSDK Example App</Text>
-      <Button title='Step 1 - Login to Siro' onPress={showModal} />
+      <Button title="Step 1 - Login to Siro" onPress={showModal} />
       <Button title="Step 2 - Start Recording!" onPress={startRecording} />
 
       <Text style={{ marginBottom: 20 }}>Test Events</Text>
-      <Button title="Step 3 - Send event!" onPress={() => sendEvent('Random Event', interaction)} />
-      <Button title="Step 4 - Send dropPin event!" onPress={() => sendEvent('dropPin', interaction)} />
-      <Button title="Step 5 - Send createLead event!" onPress={() => sendEvent('createLead')} />
-      <Button title="Step 6 - Send leadClosed event!" onPress={() => sendEvent('leadClosed')} />
-      <Button title="Step 7 - Stop Recording!" onPress={() => stopRecording()} />
+      <Button
+        title="Step 3 - Send event!"
+        onPress={() => sendEvent("Random Event", interaction)}
+      />
+      <Button
+        title="Step 4 - Send dropPin event!"
+        onPress={() => sendEvent("dropPin", interaction)}
+      />
+      <Button
+        title="Step 5 - Send createLead event!"
+        onPress={() => sendEvent("createLead")}
+      />
+      <Button
+        title="Step 6 - Send leadClosed event!"
+        onPress={() => sendEvent("leadClosed")}
+      />
+      <Button
+        title="Step 7 - Stop Recording!"
+        onPress={() => stopRecording()}
+      />
 
       <SiroReactNative.SiroButton />
     </View>
@@ -75,8 +90,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
